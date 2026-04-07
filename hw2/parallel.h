c71437f466faeee0a87bf38c88fa1cd3d0f67f84
Hoyoung027 <hybyun0207@gmail.com>
2026-04-07 06:40:46 +0000

    feat : hw2 setting

---
+++ hw2/parallel.h
@@ -0,0 +1,65 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdlib>
+#include <numeric>
+// You cannot use OpenMP <omp.h>
+// Include header files if you need,
+// but it must work without modifying the Makefile
+
+/**
+ * @brief Initializes a vector with random float values.
+ *
+ * This function fills the given array
+ *
+ * @param a Pointer to the array to be initialized.
+ * @param N The number of elements in the array.
+ */
+inline void init_vec(float *a, int N) {
+	/****************/
+	/* TODO: put your own parallelized code here */
+	/* You don't have to parallelize all of your code - it's up to you. */
+
+	/****************/
+}
+
+/**
+ * @brief Performs a matrix-vector multiplication.
+ *
+ * This function computes the product of a matrix 'a' and a vector 'b', storing
+ * the result in vector 'c'.
+ *
+ * @param a Pointer to the first element of the matrix 'a' (assumed to be in
+ * row-major order).
+ * @param b Pointer to the first element of the vector 'b'.
+ * @param c Pointer to the first element of the result vector 'c'.
+ * @param N The dimension of the matrix and vectors (assuming a square matrix
+ * and compatible vector sizes).
+ */
+inline void gemv(float *a, float *b, float *c, int N) {
+	/****************/
+	/* TODO: put your own parallelized code here */
+	/* You don't have to parallelize all of your code - it's up to you. */
+
+	/****************/
+}
+
+/**
+ * @brief Performs matrix multiplication of two NxN matrices.
+ *
+ * This function computes the product of two square matrices `a` and `b`,
+ * and stores the result in matrix `c`. All matrices are represented as
+ * 1-dimensional arrays in row-major order.
+ *
+ * @param a Pointer to the first input matrix (NxN).
+ * @param b Pointer to the second input matrix (NxN).
+ * @param c Pointer to the output matrix (NxN) where the result will be stored.
+ * @param N The dimension of the matrices (number of rows and columns).
+ */
+inline void gemm(float *a, float *b, float *c, int N) {
+	/****************/
+	/* TODO: put your own parallelized code here */
+	/* You don't have to parallelize all of your code - it's up to you. */
+
+	/****************/
+}
