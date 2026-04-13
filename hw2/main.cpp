c7bc8e241f77a6081601ad3a980fadee748e9a12
Hoyoung027 <hybyun0207@gmail.com>
2026-04-13 13:42:19 +0000

    hw 2 : Basic Setting Updated

--- hw2/main.cpp
+++ hw2/main.cpp
@@ -1,4 +1,4 @@
-#include "parallel.h"
+#include "parallel-double.h"
 
 #include <algorithm>
 #include <cassert>
@@ -6,25 +6,31 @@
 #include <cmath>
 #include <cstdlib>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <numeric>
 #include <stdlib.h>
 
 using namespace std;
 
-bool is_same_vec(float *a, float *b, int N) {
+bool is_same_vec(double *a, double *b, int N) {
 	for (int i = 0; i < N; i++) {
-		if (abs(a[i] - b[i]) > 1e-7) {
+		if (abs(a[i] - b[i]) > 1e-2) {
+			cout << std::setprecision(17) << "Mismatch at (" << i << "): " << a[i]
+				 << " vs " << b[i] << " (diff=" << (a[i] - b[i]) << ")" << endl;
 			return false;
 		}
 	}
 	return true;
 }
 
-bool is_same_mat(float *a, float *b, int N) {
+bool is_same_mat(double *a, double *b, int N) {
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
-			if (abs(a[i * N + j] - b[i * N + j]) > 1e-7) {
+			if (abs(a[i * N + j] - b[i * N + j]) > 1e-3) {
+				cout << std::setprecision(17) << "Mismatch at (" << i << ", " << j
+					 << "): " << a[i * N + j] << " vs " << b[i * N + j]
+					 << " (diff=" << (a[i * N + j] - b[i * N + j]) << ")" << endl;
 				return false;
 			}
 		}
@@ -35,11 +41,11 @@ bool is_same_mat(float *a, float *b, int N) {
 int main(int argc, char **argv) {
 	const int N = 1 << 11;
 
-	float *mat_a = new float[N * N];
-	float *mat_b = new float[N * N];
-	float *vec_b = new float[N];
-	float *mat_c = new float[N * N];
-	float *vec_c = new float[N];
+	double *mat_a = new double[N * N];
+	double *mat_b = new double[N * N];
+	double *vec_b = new double[N];
+	double *mat_c = new double[N * N];
+	double *vec_c = new double[N];
 
 	fstream f_mat_a("/data/hw2/matrix_a.txt", ios::in);
 	fstream f_mat_b("/data/hw2/matrix_b.txt", ios::in);
@@ -59,10 +65,10 @@ int main(int argc, char **argv) {
 
 	// 1. Parallel GEMM Approach
 	{
-		std::chrono::duration<float> diff;
+		std::chrono::duration<double> diff;
 		auto start = std::chrono::steady_clock::now();
 		{
-			float *gemm_out = new float[N * N];
+			double *gemm_out = new double[N * N];
 			gemm(mat_a, mat_b, gemm_out, N);
 			if (!is_same_mat(gemm_out, mat_c, N)) {
 				cerr << "Parallel GEMM Approach Failed" << endl;
@@ -78,15 +84,15 @@ int main(int argc, char **argv) {
 
 	// 2. Parallel Freivalds’ Algorithm
 	{
-		float *my_vec = new float[N];
+		double *my_vec = new double[N];
 		init_vec(my_vec, N);
 
-		std::chrono::duration<float> diff;
+		std::chrono::duration<double> diff;
 		auto start = std::chrono::steady_clock::now();
 		{
-			float *gemv_out1 = new float[N];
-			float *gemv_out2 = new float[N];
-			float *buf = new float[N];
+			double *gemv_out1 = new double[N];
+			double *gemv_out2 = new double[N];
+			double *buf = new double[N];
 
 			gemv(mat_b, my_vec, buf, N);
 			gemv(mat_a, buf, gemv_out1, N);
