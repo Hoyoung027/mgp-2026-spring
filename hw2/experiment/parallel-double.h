86a8f3e6ff350b9f638fea1eef2f06475ed1827d
Hoyoung027 <hybyun0207@gmail.com>
2026-04-13 23:33:00 +0900

    hw2 : naive GEMV & GEMM

--- hw2/experiment/parallel-double.h
+++ hw2/experiment/parallel-double.h
@@ -3,9 +3,6 @@
 #include <cmath>
 #include <cstdlib>
 #include <numeric>
-#include <pthread.h>
-#include <thread>
-#include <vector>
 // You cannot use OpenMP <omp.h>
 // Include header files if you need,
 // but it must work without modifying the Makefile
@@ -48,31 +45,12 @@ inline void gemv(double *a, double *b, double *c, int N) {
 	/* TODO: put your own parallelized code here */
 	/* You don't have to parallelize all of your code - it's up to you. */
 
-	struct Args { double *a, *b, *c; int N, start, end; };
-	struct Worker {
-		static void *run(void *arg) {
-			auto *p = static_cast<Args *>(arg);
-			for (int i = p->start; i < p->end; ++i) {
-				double sum = 0.0;
-				for (int j = 0; j < p->N; ++j)
-					sum += p->a[i * p->N + j] * p->b[j];
-				p->c[i] = sum;
-			}
-			return nullptr;
-		}
-	};
-
-	int nt = std::min((int)std::thread::hardware_concurrency(), 32);
-	std::vector<Args> args(nt);
-	std::vector<pthread_t> threads(nt);
-	int chunk = (N + nt - 1) / nt;
-
-	for (int t = 0; t < nt; ++t) {
-		args[t] = {a, b, c, N, t * chunk, std::min((t + 1) * chunk, N)};
-		pthread_create(&threads[t], nullptr, Worker::run, &args[t]);
+	for (int i = 0; i < N; ++i) {
+		double sum = 0.0;
+		for (int j = 0; j < N; ++j)
+			sum += a[i * N + j] * b[j];
+		c[i] = sum;
 	}
-	for (int t = 0; t < nt; ++t)
-		pthread_join(threads[t], nullptr);
 
 	/****************/
 }
@@ -94,43 +72,16 @@ inline void gemm(double *a, double *b, double *c, int N) {
 	/* TODO: put your own parallelized code here */
 	/* You don't have to parallelize all of your code - it's up to you. */
 
-	struct Args { double *a, *b, *c; int N, start, end; };
-	struct Worker {
-		static void *run(void *arg) {
-			constexpr int TILE = 48;
-			auto *p = static_cast<Args *>(arg);
-			double *a = p->a, *b = p->b, *c = p->c;
-			int N = p->N;
+	for (int i = 0; i < N; ++i)
+		for (int j = 0; j < N; ++j)
+			c[i * N + j] = 0.0;
 
-			for (int i = p->start; i < p->end; ++i)
-				for (int j = 0; j < N; ++j)
-					c[i * N + j] = 0.0;
-
-			for (int ii = p->start; ii < p->end; ii += TILE)
-				for (int kk = 0; kk < N; kk += TILE)
-					for (int jj = 0; jj < N; jj += TILE)
-						for (int i = ii; i < std::min(ii + TILE, p->end); ++i)
-							for (int k = kk; k < std::min(kk + TILE, N); ++k) {
-								double aik = a[i * N + k];
-								int jend = std::min(jj + TILE, N);
-								for (int j = jj; j < jend; ++j)
-									c[i * N + j] += aik * b[k * N + j];
-							}
-			return nullptr;
+	for (int i = 0; i < N; ++i)
+		for (int k = 0; k < N; ++k) {
+			double aik = a[i * N + k];
+			for (int j = 0; j < N; ++j)
+				c[i * N + j] += aik * b[k * N + j];
 		}
-	};
-
-	int nt = (int)std::thread::hardware_concurrency();
-	std::vector<Args> args(nt);
-	std::vector<pthread_t> threads(nt);
-	int chunk = (N + nt - 1) / nt;
-
-	for (int t = 0; t < nt; ++t) {
-		args[t] = {a, b, c, N, t * chunk, std::min((t + 1) * chunk, N)};
-		pthread_create(&threads[t], nullptr, Worker::run, &args[t]);
-	}
-	for (int t = 0; t < nt; ++t)
-		pthread_join(threads[t], nullptr);
 
 	/****************/
 }
